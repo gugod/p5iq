a3c607f9dcab4c1ef47bd8e5b482798d53d8f489
Kang-min Liu <gugod@gugod.org>
2015-07-30 22:37:17 +0200

    put the analyzation routines together.

--- lib/P5iq.pm
+++ lib/P5iq.pm
@@ -15,4 +15,64 @@ sub es {
     return $es;
 }
 
+sub analyze_for_index {
+    my ($ppi_doc) = @_;
+
+    my @doc;
+    for my $statement (@{ $ppi_doc->find('PPI::Statement') ||[] }) {
+        my $location = $statement->location;
+        my $doc = {
+            location      => join("\0",@{$location}[0,1]),
+            content       => $statement->content,
+            class         => $statement->class,
+            token_content => [],
+            token_class   => [],
+            tags          => [],
+        };
+        for my $c ($statement->schildren) {
+            push @{$doc->{token_content}}, $c->content;
+            push @{$doc->{token_class}}, $c->class;
+        }
+        push @doc, $doc;
+    }
+
+    for my $s (@{ $ppi_doc->find('PPI::Structure::Subscript') ||[] }) {
+        my @c = ( $s );
+        my $p = $s;
+        while ($p = $p->sprevious_sibling) {
+            unshift @c, $p;
+            last if $p->isa("PPI::Token::Symbol");
+        }
+        my $location = $c[0]->location;
+        my $doc = {
+            location      => join("\0",@{$location}[0,1]),
+            content       => join("", @c),
+            class         => 'PPI::Structure::Subscript',
+            token_content => [map { $_->content } @c],
+            token_class   => [map { $_->class }   @c],
+            tags          => [
+                "subscript:symbol=$c[0]",
+                "subscript:container=" . join("", map { $_->content } @c[0..$#c-1] ),
+                "subscript:content=$s",
+            ],
+        };
+        push @doc, $doc;
+    }
+    return @doc;
+}
+
+sub analyze_for_query {
+    my ($ppi_doc) = @_;
+    my @tokens = grep { $_->significant } $ppi_doc->tokens;
+
+    my $es_query = {
+        bool => {
+            must   => [ (map { +{ term => { "token_class" => $_->class } } } @tokens) ],
+            should => [ (map { +{ match => { token_content => $_->content } } } @tokens) ]
+        }
+    };
+
+    return $es_query;
+}
+
 1;
