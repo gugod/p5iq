561006f2f373cf4e5babc80ae43c8bd37c148341
Gabor Szabo <gabor@szabgab.com>
2015-11-17 12:47:20 +0200

    use fixed files for the tests that won't change as the code of the application evolves

--- t/00-test.t
+++ t/00-test.t
@@ -4,7 +4,9 @@ use v5.14;
 use warnings;
 
 use Test::More;
+use Test::Deep;
 use Capture::Tiny qw(capture);
+use LWP::Simple qw(get);
 
 plan tests => 2;
 
@@ -15,22 +17,62 @@ END {
 
 use P5iq::Index;
 use P5iq::Search;
+use P5iq;
 
 diag "P5IQ_INDEX: $ENV{P5IQ_INDEX}";
 is P5iq::idx(), $ENV{P5IQ_INDEX}, 'idx() as expected';
 
 # this call is forking which breaks the END block so for now we call the external command.
 # P5iq::Index::index_dirs('lib');
-system $^X, 'bin/p5iq-index', 'lib';
+#diag get 'http://127.0.0.1:9200/_stats?pretty=1';
+system $^X, 'bin/p5iq-index', 't/corpus';
+sleep 4; # TODO: It seems after the index script finished we still have to wait for the indexing to really be done
+my $ID = re('^[\w-]{22,}$');
+my $SCORE = re('^\d\.\d+$');
 
-my ($stdout, $stderr, $exit) = capture {
-    P5iq::Search::locate_symbols('unshift', 10, 1, 0);
-};
-is $stdout, "lib/P5iq.pm:105: unshift\nlib/P5iq.pm:176: unshift\n";
+{
+    my $res = P5iq::Search::_locate_symbols('unshift', 10, 1, 0);
+    cmp_deeply $res, bag(
+         {
+           '_id' => $ID,
+           '_index' => $ENV{P5IQ_INDEX},
+           '_score' => $SCORE,
+           '_source' => {
+             'class' => 'PPI::Token::Word',
+             'content' => 'unshift',
+             'file' => 't/corpus/lib/P5iq.pm',
+             'line_number' => 110,
+             'row_number' => 13,
+             'tags' => []
+           },
+           '_type' => 'p5_node'
+         },
+         {
+           '_id' => $ID,
+           '_index' => $ENV{P5IQ_INDEX},
+           '_score' => $SCORE,
+           '_source' => {
+             'class' => 'PPI::Token::Word',
+             'content' => 'unshift',
+             'file' => 't/corpus/lib/P5iq.pm',
+             'line_number' => 181,
+             'row_number' => 13,
+             'tags' => []
+           },
+           '_type' => 'p5_node'
+         }
+    ), '_locate_symbols unshift';
+}
+
+#my ($stdout, $stderr, $exit) = capture {
+#    P5iq::Search::locate_symbols('unshift', 10, 1, 0);
+#};
+#is $stdout, "lib/P5iq.pm:105: unshift\nlib/P5iq.pm:176: unshift\n";
+
+#diag get 'http://127.0.0.1:9200/_stats?pretty=1';
+#diag explain(P5iq->es->search( index => $ENV{P5IQ_INDEX}, body => { size => 5000 }));
 
 
-($stdout, $stderr, $exit) = capture {
-    P5iq::Search::search_p5iq_index('shift', 10);
-};
-#diag $stdout;
+my $res = P5iq::Search::_search_p5iq_index('unshift');
+#diag explain $res;
 
---
+++ t/corpus/lib/P5iq.pm
@@ -0,0 +1,239 @@
+package P5iq;
+use v5.14;
+
+use PPIx::LineToSub;
+use Elastijk;
+
+sub idx {
+    return $ENV{P5IQ_INDEX} // 'p5iq';
+}
+
+sub es {
+    state $es = do {
+        my ($es_host, $es_port) = ("localhost", "9200");
+        if (defined($ENV{P5IQ_ELASTICSEARCH_HOSTPORT})) {
+            ($es_host, $es_port) = split(":", $ENV{P5IQ_ELASTICSEARCH_HOSTPORT});
+        }
+        Elastijk->new(
+            host => $es_host,
+            port => $es_port,
+        );
+    };
+    return $es;
+}
+
+sub create_index_if_not_exist {
+    my $es = es();
+    unless ($es->exists( index => idx() )) {
+        my ($status, $res) = $es->put(
+            index => idx(),
+            body  => {
+                mappings => {
+                    p5_node => {
+                        properties => {
+                            file          => { "type" => "string", "index" => "not_analyzed" },
+                            line_number   => { "type" => "integer" },
+                            row_number    => { "type" => "integer" },
+                            class         => { "type" => "string", "index" => "not_analyzed" },
+                            content       => { "type" => "string", "index" => "not_analyzed" },
+                            token_content => { "type" => "string" },
+                            token_class   => { "type" => "string","index" => "not_analyzed" },
+                            tags          => { "type" => "string","index" => "not_analyzed" },
+                        }
+                    },
+                }
+            }
+        );
+    }
+}
+
+sub extract_token {
+    my ($ppi_doc) = @_;
+    my @doc;
+    for my $x ( $ppi_doc->tokens ) {
+        next unless $x->significant;
+        my $location = $x->location;
+        my $doc = {
+            line_number => $location->[0],
+            row_number  => $location->[1],
+            content  => $x->content,
+            class    => $x->class,
+            tags     => [],
+        };
+        if (ref($x) eq 'PPI::Token::Symbol') {
+            push @{$doc->{tags}}, (
+                'symbol:actual='    . $x->symbol,
+                'symbol:canonical=' . $x->canonical
+            );
+            if ( ref(my $x_parent = $x->parent) eq 'PPI::Statement::Variable' ) {
+                push @{$doc->{tags}}, (
+                    'in:statement:variable'
+                );
+                my $ns = $x->snext_sibling;
+                if( ref($ns) eq 'PPI::Token::Operator' && $ns->content eq '=' ){
+                    push @{$doc->{tags}}, (
+                        'in:statement:variable:defined'
+                    );
+                }
+            }
+
+            #Look for var definition like: my ($a, $b) = @_
+            if( ref($x->parent) eq 'PPI::Statement::Expression'
+                && ref($x->parent->parent) eq 'PPI::Structure::List'
+                && ref($x->parent->parent->parent) eq 'PPI::Statement::Variable'
+            ){
+                push @{$doc->{tags}}, (
+                    'in:statement:variable'
+                );
+
+                if( ref($x->parent->parent->snext_sibling) eq 'PPI::Token::Operator'
+                    && $x->parent->parent->snext_sibling->content eq '='
+                ){
+                    push @{$doc->{tags}}, (
+                        'in:statement:variable:defined'
+                    );
+                }
+            }
+        }
+        push @doc, $doc;
+    }
+    return @doc;
+}
+
+sub extract_subscript {
+    my ($ppi_doc) = @_;
+    my @doc;
+    for my $s (@{ $ppi_doc->find('PPI::Structure::Subscript') ||[] }) {
+        my @c = ( $s );
+        my $p = $s;
+        while ($p = $p->sprevious_sibling) {
+            unshift @c, $p;
+            last if $p->isa("PPI::Token::Symbol");
+        }
+        my $location = $c[0]->location;
+        my $doc = {
+            line_number   => $location->[0],
+            row_number    => $location->[1],
+            content       => join("", @c),
+            class         => 'PPI::Structure::Subscript',
+            token_content => [map { $_->content } @c],
+            token_class   => [map { $_->class }   @c],
+            tags          => [
+                "subscript:symbol=$c[0]",
+                "subscript:container=" . join("", map { $_->content } @c[0..$#c-1] ),
+                "subscript:content=$s",
+            ],
+        };
+        push @doc, $doc;
+    }
+    return @doc;
+}
+
+sub extract_function_calls {
+    my ($ppi_doc) = @_;
+    my @doc;
+
+    # Look for all "word followed by a list within parenthesis"
+    # foo(); foo(1,2,3); foo( bar(1,2,3), 4)
+    for my $s (@{ $ppi_doc->find('PPI::Token::Word') ||[]}) {
+        next unless ref($s->parent) eq 'PPI::Statement';
+
+        my $prev = $s->sprevious_sibling;
+        next if ($prev && ref($prev) eq 'PPI::Token::Operator' && $prev->content eq "->");
+
+        my $args = $s->snext_sibling;
+        next unless ref($args) eq 'PPI::Structure::List';
+
+        push @doc, {
+            line_number   => $s->location->[0],
+            row_number    => $args->location->[1],
+            content       => join("", "$s", "$args"),
+            class         => 'P5iq::FunctionCall',
+            tags          => [
+                "function:call",
+                "function:name=$s",
+                "function:arglist=$args"
+            ],
+        };
+    }
+
+    # TODO: Look for all "word followed by a list not within parenthesis"
+    # foo; foo 1,2; foo bar(1,2,3), 4;
+
+    return @doc;
+}
+
+sub extract_method_calls {
+    my ($ppi_doc) = @_;
+
+    my @doc;
+    for my $s (@{ $ppi_doc->find('PPI::Token::Operator') ||[]}) {
+        next unless $s->content eq '->';
+
+        my $method = $s->snext_sibling;
+        next unless $method->isa('PPI::Token');
+
+        my @ctxt;
+        my $p = $s;
+        while ($p = $p->sprevious_sibling) {
+            last if !$p->isa('PPI::Token') || $p->isa('PPI::Token::Operator') && $p->content ne '->';
+            last if @ctxt && $p->isa('PPI::Token::Word') && !$ctxt[0]->isa('PPI::Token::Operator');
+            unshift(@ctxt, $p);
+        }
+
+        my $context = @ctxt ? join("", @ctxt) : "???";
+
+        my $args = $method->snext_sibling;
+        $args = (ref($args) eq 'PPI::Structure::List') ? $args->content : "";
+
+        push @doc, {
+            line_number   => $method->location->[0],
+            row_number    => $method->location->[1],
+            content       => join("", "$context", "->", "$method", "$args"),
+            class         => 'P5iq::MethodCall',
+            tags          => [
+                "method:call",
+                "method:name=$method",
+                "method:context=$context",
+                "method:arglist=$args"
+            ],
+        };
+    }
+    return @doc;
+}
+
+sub analyze_for_index {
+    my ($ppi_doc) = @_;
+
+    $ppi_doc->index_line_to_sub;
+
+    my @doc;
+    push @doc, extract_token($ppi_doc);
+    push @doc, extract_subscript($ppi_doc);
+    push @doc, extract_function_calls($ppi_doc);
+    push @doc, extract_method_calls($ppi_doc);
+    return @doc;
+}
+
+sub analyze_for_query {
+    my ($ppi_doc) = @_;
+    my @tokens = grep { $_->significant } $ppi_doc->tokens;
+
+    my $es_query = {
+        bool => {
+            must   => [ (map { +{ term => { "token_class" => $_->class } } } @tokens) ],
+            should => [ (map { +{ match => { token_content => $_->content } } } @tokens) ]
+        }
+    };
+
+    return $es_query;
+}
+
+sub delete_all {
+    my $es = es();
+    $es->delete(
+        index => idx(),
+    );
+}
+
+1;
